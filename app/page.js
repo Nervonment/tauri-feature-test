382d182f1a33d82768e574debf20dbd2788305fe
Nervonment <nervonment@outlook.com>
2024-06-05 17:22:05 +0800

    v0.1.2

--- app/page.js
+++ app/page.js
@@ -9,7 +9,7 @@ export default function Home() {
           <code className="font-mono font-bold">app/page.js</code>
         </p>
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          v0.1.1
+          v0.1.2
         </div>
       </div>
 
